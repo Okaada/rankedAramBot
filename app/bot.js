352a81039ddbea984f8ae26c1764c756415dd13e
Ravel Okada <ravelokada@gmail.com>
2023-04-27 20:28:19 -0300

    Adjust node version for project

--- app/bot.js
+++ app/bot.js
@@ -2,22 +2,21 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const _ = require('lodash');
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
-const client = new Client({
+const client = new Discord.Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
     ]
-})
+  });
 
 client.on('ready', () => {
     console.log(`Bot ${client.user.tag} está pronto para ser usado!`);
 });
 
 // Evento que é executado quando o bot recebe uma mensagem
-client.on('messageCreate', async (message) => {
+client.on('message', async (message) => {
     // Verificando se a mensagem foi enviada por um bot
     if (message.author.bot) return;
 
@@ -32,37 +31,37 @@ client.on('messageCreate', async (message) => {
             message.channel.send(`Boa Gameplay, não se esqueçam das regras: \n\n1- Ofender todos \n2- Não se matar pra torre\n3- Ofender todos\n4- Acertou bolinha de neve......... VAI\n5- Ofender todos (Não se esqueça do seu próprio time)\n6- Não de dodge \n7- Divirta-se`);
         }).catch()
     }
-    if(command[1] == 'regras')
+    if (command[1] == 'regras')
         message.channel.send(`O Livro das regras ainda não ta 100% pronto, mas na moral não se mata pra torre bobão.`);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 function createTeam() {
-   return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         client.on('messageCreate', (messageStart) => {
             let matcher = messageStart.content.match("^([a-zA-Z0-9]+\/)+[a-zA-Z0-9]+$")
             if (matcher == null)
                 return;
 
             const names = matcher[0].split('/'); // Separa os names em uma lista
-            if(names.length % 2 === 0){
+            if (names.length % 2 === 0) {
                 const sortedNames = _.shuffle(names); // Embaralha aleatoriamente os names
                 const half = Math.ceil(sortedNames.length / 2);
-                
+
                 const teamOneList = sortedNames.slice(0, half);
                 const teamTwoList = sortedNames.slice(half);
-                
+
                 let teamOneNames = teamOneList.join(", ");
                 let teamTwoNames = teamTwoList.join(", ");
                 messageStart.channel.send(`Time 1: ${teamOneNames} \nTime 2: ${teamTwoNames}`)
                 resolve();
             }
-            else{
+            else {
                 messageStart.channel.send(`Infezlimente não quero fazer parte dessa crocodilagem de um time ficar -1, ainda mais se o Guijas tiver `)
                 reject()
             }
-            
+
         });
     })
 }
