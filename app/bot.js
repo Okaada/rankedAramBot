f72f9eb79813974f9c773d88f3916b8bf2663b3e
Ravel Okada <ravelokada@gmail.com>
2023-04-27 19:58:56 -0300

    fixed join list

--- app/bot.js
+++ app/bot.js
@@ -23,17 +23,17 @@ client.on('messageCreate', async (message) => {
 
     //Verificando se a mensagem começa com o prefixo '!aram'
     if (!message.content.startsWith('!aram')) return;
-
     let command = message.content.split(' ')
-    console.log(command[1])
 
     if (command[1] == 'start') {
         message.reply("Quem serão os azarados que vão jogar com o Wesley ou Guijas? \nPasse os Nicks no seguinte padrão: Barbixinha/Lascaltinho/Patriquinho/...")
 
         createTeam().then(x => {
-            message.channel.send(`Boa Gameplay, não se esqueçam das regras \n1- Ofender todos \n2- Não se matar pra torre\n3- Ofender todos\n4- Acertou bolinha de neve......... VAI\n5- Ofender todos (Não se esqueça do seu próprio time)\n6- Não de dodge \n7- Divirta-se`);
-        }).catch(err => message.channel.send('Hmmmm, acho que alguma coisa ta mal programda'))
+            message.channel.send(`Boa Gameplay, não se esqueçam das regras: \n\n1- Ofender todos \n2- Não se matar pra torre\n3- Ofender todos\n4- Acertou bolinha de neve......... VAI\n5- Ofender todos (Não se esqueça do seu próprio time)\n6- Não de dodge \n7- Divirta-se`);
+        }).catch()
     }
+    if(command[1] == 'regras')
+        message.channel.send(`O Livro das regras ainda não ta 100% pronto, mas na moral não se mata pra torre bobão.`);
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
@@ -45,15 +45,24 @@ function createTeam() {
             if (matcher == null)
                 return;
 
-            const nomes = matcher[0].split('/'); // Separa os nomes em uma lista
-            const nomesEmbaralhados = _.shuffle(nomes); // Embaralha aleatoriamente os nomes
-            const metade = Math.ceil(nomesEmbaralhados.length / 2);
-
-            const lista1 = nomesEmbaralhados.slice(0, metade);
-            const lista2 = nomesEmbaralhados.slice(metade);
-
-            messageStart.channel.send(`Time 1: ${lista1[0]}, ${lista1[1]}, ${lista1[2]} \nTime 2: ${lista2[0]}, ${lista2[1]}, ${lista2[2]}`)
-            resolve();
+            const names = matcher[0].split('/'); // Separa os names em uma lista
+            if(names.length % 2 === 0){
+                const sortedNames = _.shuffle(names); // Embaralha aleatoriamente os names
+                const half = Math.ceil(sortedNames.length / 2);
+                
+                const teamOneList = sortedNames.slice(0, half);
+                const teamTwoList = sortedNames.slice(half);
+                
+                let teamOneNames = teamOneList.join(", ");
+                let teamTwoNames = teamTwoList.join(", ");
+                messageStart.channel.send(`Time 1: ${teamOneNames} \nTime 2: ${teamTwoNames}`)
+                resolve();
+            }
+            else{
+                messageStart.channel.send(`Infezlimente não quero fazer parte dessa crocodilagem de um time ficar -1, ainda mais se o Guijas tiver `)
+                reject()
+            }
+            
         });
     })
 }
